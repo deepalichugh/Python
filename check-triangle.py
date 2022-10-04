49a0461280c5307aab7336ffc37acc4f94495fcb
deepalichugh <chugh@deepali.com>
2022-10-03 21:44:49 -0400

    Updated logic for isosceles

--- check-triangle.py
+++ check-triangle.py
@@ -3,7 +3,7 @@ side2 = int(input("Enter second side"));
 side3 = int(input("Enter third side"));
 if side1 == side2 and side1 == side3:
   print(1) #Equilateral
-elif ((side1 == side2 and side1 != side3) or (side1 == side3 and side2 != side1) or (side3 == side1 and side3 != side2)):
+elif ((side1 == side2 and side1 != side3) or (side1 == side3 and side1 != side2) or (side2 == side3 and side2 != side1)):
   print(0)  #Isosceles
 else:
   print(-1) #Scalene
