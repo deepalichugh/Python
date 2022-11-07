928c176f917267910bf00dcef9201b745e421876
deepalichugh <chugh@deepali.com>
2022-11-07 16:56:29 -0500

    Average of three numbers

--- find_avg_of_three.py
+++ find_avg_of_three.py
@@ -2,8 +2,8 @@ def avg(a, b, c):
     output = (a+b+c)/3
     return output    
 
-a =  int(input())
-b=int(input())
-c=int(input())
+a = int(input())
+b = int(input())
+c = int(input())
 
 print(avg(a,b,c))
